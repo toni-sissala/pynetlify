aa270aaaf6b71309b31ce9b07704d16e26dca42d
Toni Sissala <toni.sissala@gmail.com>
2019-03-23 23:04:10 +0200

    Improve cli. Add support for deploying. Add .gitignore.

--- pynetlify/cli.py
+++ pynetlify/cli.py
@@ -1,10 +1,20 @@
-import sys
+import os
+import logging
 import argparse
+import configparser
 from pynetlify import pynetlify
 
 
+def deploy_folder(netlify_api, args):
+    netlify_api.deploy_folder_to_site(
+        args.folder,
+        pynetlify.Site(name=None, url=None, id=args.site_id),
+        args.force_all)
+
+
 def create_site(netlify_api, args):
-    site = netlify_api.create_site()
+    site = netlify_api.create_site(name=args.name,
+                                   custom_domain=args.domain)
     print(site)
 
 
@@ -19,25 +29,54 @@ def delete_site(netlify_api, args):
         id=args.site_id))
 
 
+def delete_all_sites(netlify_api, args):
+    for site in netlify_api.sites():
+        netlify_api.delete_site(site)
+
+
 def cli_main():
-    parser = argparse.ArgumentParser(description='Interact with Netlify API.')
-    parser.add_argument('--auth-token', required=True, type=str,
-                        help='Netlify Authentication Token')
-    subparsers = parser.add_subparsers(help='subcmd help',
-                                       dest='action')
-    subparsers.add_parser('create_site')
-    subparsers.add_parser('list_sites')
-    delete_site_parser = subparsers.add_parser('delete_site')
-    delete_site_parser.add_argument('site_id', type=str)
-    args = parser.parse_args()
+    argparser = argparse.ArgumentParser(
+        description='Interact with Netlify API.')
+    subparsers = argparser.add_subparsers(help='action help', dest='action')
+    subparsers.required = True
+    argparser.add_argument('--auth-token', type=str,
+                           help='Netlify Authentication Token')
+    argparser.add_argument('--loglevel', default='INFO', choices=[
+        'DEBUG', 'INFO', 'WARN', 'ERROR'])
     available_actions = {'create_site': create_site,
                          'delete_site': delete_site,
+                         'delete_all_sites': delete_all_sites,
+                         'deploy_folder': deploy_folder,
                          'list_sites': list_sites}
+    deploy_folder_parser = subparsers.add_parser('deploy_folder')
+    deploy_folder_parser.add_argument('--site-id', required=True,
+                                      type=str)
+    deploy_folder_parser.add_argument('--force-all', action='store_true')
+    deploy_folder_parser.add_argument('folder', type=str)
+    create_site_parser = subparsers.add_parser('create_site')
+    create_site_parser.add_argument('--name', type=str,
+                                    help='Site name')
+    create_site_parser.add_argument('--domain', type=str,
+                                    help='Site domain')
+    subparsers.add_parser('list_sites')
+    delete_site_parser = subparsers.add_parser('delete_site')
+    subparsers.add_parser('delete_all_sites')
+    delete_site_parser.add_argument('site_id', type=str)
+    args = argparser.parse_args()
+    mod_dir = os.path.dirname(__file__)
+    cwd = os.getcwd()
+    conf_filename = 'pynetlify.ini'
+    configfile_paths = [
+        os.path.join(cwd, conf_filename),
+        os.path.join(mod_dir, conf_filename)]
+    config = configparser.ConfigParser()
+    for c_filepath in configfile_paths:
+        if os.path.exists(c_filepath):
+            config.read(c_filepath)
+    # TODO: inform user of missing auth-token
+    auth_token = args.auth_token or config.get('netlify', 'auth-token')
+    logging.basicConfig(level=getattr(logging, args.loglevel))
     selected_action = available_actions.get(args.action)
-    if not selected_action:
-        print("Select action: %s" %
-              (', '.join(available_actions.keys()),))
-        return 1
-    netlify_api = pynetlify.APIRequest(args.auth_token)
+    netlify_api = pynetlify.APIRequest(auth_token)
     selected_action(netlify_api, args)
     return 0
--- pynetlify/pynetlify.py
+++ pynetlify/pynetlify.py
@@ -1,10 +1,19 @@
+import os
 from collections import namedtuple
+import urllib
+import hashlib
+import glob
+import logging
+import pprint
 import requests
 
+
 Site = namedtuple('Site', ['name', 'id', 'url'])
+logger = logging.getLogger(__name__)
 
 
 def rdict_to_site(rdict):
+    logger.debug(pprint.pformat(rdict))
     return Site(name=rdict['name'], id=rdict['id'], url=rdict['url'])
 
 
@@ -23,17 +32,17 @@ class APIRequest:
 
     def sites(self):
         url = self._auth_url('v1', 'sites')
-        response = requests.get(url, self.headers)
+        response = requests.get(url, headers=self.headers)
         response.raise_for_status()
         for site in response.json():
             yield rdict_to_site(site)
 
-    def create_site(self):
-        response = requests.post(self._auth_url('v1', 'sites'), self.headers)
+    def create_site(self, **site_properties):
+        response = requests.post(self._auth_url('v1', 'sites'), json=site_properties, headers=self.headers)
         response.raise_for_status()
         if response.status_code != 201:
-            raise Exception('Invalid response status_code %s'
-                            % (response.status_code,))
+            logger.warning('Unexpected response status code %s'
+                           % (response.status_code,))
         return rdict_to_site(response.json())
 
     def delete_site(self, site):
@@ -41,6 +50,48 @@ class APIRequest:
         response = requests.delete(url, headers=self.headers)
         response.raise_for_status()
         if response.status_code != 200:
-            raise Exception('Invalid response status_code %s'
-                            % (response.status_code,))
+            logger.warning('Unexpected response status code %s'
+                           % (response.status_code,))
         return True
+
+    def deploy_folder_to_site(self, folder, site, force_all=False):
+        lookup_path = os.path.join(folder, '**')
+        files_hashes = {}
+        for filepath in glob.iglob(lookup_path, recursive=True):
+            if not os.path.isfile(filepath):
+                continue
+            logger.debug('Preparing hash of %s', filepath)
+            with open(filepath, 'rb') as filehandle:
+                files_hashes.update({
+                    filepath.replace(folder, '', 1):
+                    hashlib.sha1(filehandle.read()).hexdigest()
+                })
+        logger.debug('Requesting required hashes of files %s',
+                     ', '.join(files_hashes.keys()))
+        response = requests.post(self._auth_url(
+            'v1', 'sites', site.id, 'deploys'),
+                                 json={'files': files_hashes},
+                                 headers=self.headers)
+        response.raise_for_status()
+        response_json = response.json()
+        logger.debug(pprint.pformat(response_json))
+        required_hashes = response_json['required']
+        logger.debug('Required filehashes: %s', required_hashes)
+        if not required_hashes and not force_all:
+            return
+        hashes_files = {value: key for (key, value) in files_hashes.items()}
+        deploy_id = response_json['id']
+        deploy_headers = self.headers.copy()
+        deploy_headers.update({'Content-Type': 'application/octet-stream'})
+        if force_all:
+            required_hashes = files_hashes.values()
+        for required_hash in required_hashes:
+            filepath = folder + hashes_files[required_hash]
+            with open(filepath, 'rb') as filehandle:
+                response = requests.put(
+                    self._auth_url('v1', 'deploys',
+                                   deploy_id, 'files',
+                                   urllib.parse.quote(hashes_files[required_hash])),
+                    data=filehandle,
+                    headers=deploy_headers)
+                response.raise_for_status()
