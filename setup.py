507f5b5e980f402ce0b643809f5dc52b5b0c524d
Toni Sissala <toni.sissala@gmail.com>
2019-03-26 21:15:43 +0200

    Add license and requirements.txt

--- setup.py
+++ setup.py
@@ -1,4 +1,20 @@
 #!/usr/bin/env python3
+#
+# PyNetlify - Python client and library to interact with Netlify API.
+# Copyright (C) 2019  Toni Sissala
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 from setuptools import setup, find_packages
 
@@ -13,5 +29,15 @@ setup(
     packages=find_packages(),
     install_requires=[
         'requests'
-    ]
+    ],
+    license='GPL v3',
+    classifiers=(
+        "Development Status :: 3 - Alpha",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Programming Language :: Python",
+        "Topic :: Internet :: WWW/HTTP :: Site Management",
+        "Topic :: Software Development :: Libraries :: Python Modules"
+    )
 )
